fe956f6ca5ab88b267ffacac63f2ab186696a913
Younes Mel <103428027+Youn35-Mel@users.noreply.github.com>
2022-12-06 12:09:23 +0000

    client login register page and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Navbar from "./components/Navbar/Navbar";
+import LeftBar from "./components/LeftBar/LeftBar";
+import RightBar from "./components/RightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import { element } from "prop-types";
 
 function App() {
+  const currentUser = false;
+
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile.:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/Register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+        </Routes>
+      </BrowserRouter> */}
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import Stories from "../../components/Stories/Stories";
+import Posts from "../../components/Posts/Posts";
+import Share from "../../components/Share/Share";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Stories />
+      <Share />
+      <Posts />
+    </div>
+  );
+};
+
+export default Home;
