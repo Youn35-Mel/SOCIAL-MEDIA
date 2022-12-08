8a7b407ae38fdfc276cab2d7fafe5e23c9af4d61
Younes Mel <103428027+Youn35-Mel@users.noreply.github.com>
2022-12-08 00:06:10 +0000

    add post from client

--- client/src/components/Share/Share.js
+++ client/src/components/Share/Share.js
@@ -2,11 +2,35 @@ import "./Share.scss";
 import Image from "../../assets/img.png";
 import Map from "../../assets/map.png";
 import Friend from "../../assets/friend.png";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../components/context/AuthContext";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
 
 const Share = () => {
   const { currentUser } = useContext(AuthContext);
+
+  const [file, setFile] = useState(null);
+  const [desc, setDesc] = useState("");
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (newPost) => {
+      return makeRequest.post("/posts", newPost);
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(["posts"]);
+      },
+    }
+  );
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    mutation.mutate({ desc });
+  };
   return (
     <div className="share">
       <div className="container">
@@ -15,12 +39,18 @@ const Share = () => {
           <input
             type="text"
             placeholder={`What's on your mind ${currentUser.name}?`}
+            onChange={(e) => setDesc(e.target.value)}
           />
         </div>
         <hr />
         <div className="bottom">
           <div className="left">
-            <input type="file" id="file" style={{ display: "none" }} />
+            <input
+              type="file"
+              id="file"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
             <label htmlFor="file">
               <div className="item">
                 <img src={Image} alt="" />
@@ -37,7 +67,7 @@ const Share = () => {
             </div>
           </div>
           <div className="right">
-            <button>Share</button>
+            <button onClick={handleClick}>Share</button>
           </div>
         </div>
       </div>
